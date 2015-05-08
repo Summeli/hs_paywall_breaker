9bec7450717c88a5d57147fb60f9c2748e715171
Antti Pohjola <summeli@summeli.fi>
2015-05-08 09:41:33 +0300

    updated paywall breaker

--- hs_breaker.js
+++ hs_breaker.js
@@ -1,1 +1,13 @@
-localStorage.removeItem('_hs_hist');
+document.addEventListener ("DOMContentLoaded", DOM_ContentReady);
+window.addEventListener ("load", pageFullyLoaded);
+$("html").removeClass("no-js");
+
+function DOM_ContentReady () {
+	//we have nothing to do in here
+}
+
+function pageFullyLoaded () {
+	//remove localstorage
+	window.localStorage.clear("_hs_paywall_hits");
+	window.localStorage.clear("_hs_hist");
+}
